92f9b59bbab50f39240305cd09a907493f1b6e97
osu <joanosuna9@gmail.com>
2021-11-22 22:28:19 +0100

    Added error test

--- server/middlewares/error.ts
+++ server/middlewares/error.ts
@@ -2,21 +2,16 @@ const { ValidationError } = require("express-validation");
 
 const debug = require("debug")("escroom:errors");
 
-const notFoundErrorHandler = (req, res) => {
+export const notFoundErrorHandler = (req, res) => {
   res.status(404).json({ error: "Endpoint not found" });
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const generalErrorHandler = (error, req, res, next) => {
+export const generalErrorHandler = (error, req, res, next) => {
   debug("An error has occurred: ", error.message);
   if (error instanceof ValidationError) {
     return res.status(error.statusCode).json(error.message);
   }
   const message = error.code ? error.message : "General pete";
   res.status(error.code || 500).json({ error: message });
-};
-
-export = {
-  notFoundErrorHandler,
-  generalErrorHandler,
 };
