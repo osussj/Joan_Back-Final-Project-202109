f796682e4a88f352164089ac0919ec6849911acb
osu <joanosuna9@gmail.com>
2021-11-22 16:15:58 +0100

    Added main express config

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+  return res.status(200).send({
+    message: "It works!",
+  });
+});
+
+try {
+  app.listen(port, (): void => {
+    console.log(`Connected successfully on port ${port}`);
+  });
+} catch (error) {
+  console.error(`Error occured:`);
+}
