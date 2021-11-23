79fdb568e707285e148a2cb111b5155c504e2ccc
osu <joanosuna9@gmail.com>
2021-11-23 19:15:39 +0100

    Added /profile endpoint with getUser function

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
-import { createUser, loginUser } from "../controller/userController";
+import { createUser, loginUser, getUser } from "../controller/userController";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/login", loginUser);
+router.get("/profile", auth, getUser);
 
 export default router;
