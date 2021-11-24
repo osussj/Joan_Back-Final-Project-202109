cb368f5a17c760a5d9fa44db19cc147b64b7ddaa
osu <joanosuna9@gmail.com>
2021-11-24 20:57:23 +0100

    Added getQuestion and updateQuestion test of challengeController

--- server/controller/challengeController.test.ts
+++ server/controller/challengeController.test.ts
@@ -1,7 +1,9 @@
 import { Request } from "express";
 import Question from "../../database/models/question";
 import { mockResponse } from "../../utils/mocks/mockFunction";
-import createQuestion from "./challengeController";
+import { createQuestion, getQuestion } from "./challengeController";
+
+jest.mock("../../database/models/question");
 
 describe("Given a createQuestion function", () => {
   describe("When it receives a request with the question information", () => {
@@ -40,6 +42,41 @@ describe("Given a createQuestion function", () => {
       expect(next).toHaveBeenCalled();
       expect(error).toHaveProperty("error", "Bad question provided");
       expect(error.code).toBe(400);
+    });
+  });
+});
+
+describe("Given a getQuestion function", () => {
+  describe("When it receives an array of platforms", () => {
+    test("Then it should invoke the method json with the platforms", async () => {
+      const questions = [
+        {
+          question: "What is the mark password",
+          answer: "snowflake",
+          hint: "Remember to look at the source code",
+        },
+        {
+          question: "What is the guest password?",
+          answer: "pepe",
+          hint: "There is no hint",
+        },
+      ];
+      const res = mockResponse();
+      Question.find = jest.fn().mockResolvedValue(questions);
+
+      await getQuestion(null, res, null);
+
+      expect(res.json).toHaveBeenCalledWith(questions);
+    });
+  });
+  describe("When the promise is rejected", () => {
+    test("Then it should invoke next with the error", async () => {
+      const next = jest.fn();
+
+      Question.find = jest.fn().mockRejectedValue(null);
+      await getQuestion(null, null, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
