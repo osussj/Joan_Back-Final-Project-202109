77a57132728f70c80a6f228744b7378c1b720324
osu <joanosuna9@gmail.com>
2021-11-25 15:51:54 +0100

    Added /node route with getChallenge function to roomRoutes

--- server/routes/roomRoutes.ts
+++ server/routes/roomRoutes.ts
@@ -4,6 +4,7 @@ import {
   updateQuestion,
   getQuestion,
   deleteQuestion,
+  getChallenge,
 } from "../controller/challengeController";
 import auth from "../middlewares/auth";
 
@@ -13,5 +14,7 @@ router.get("/node/question", auth, getQuestion);
 router.post("/node/question", auth, createQuestion);
 router.put("/node/question", auth, updateQuestion);
 router.delete("/node/question", auth, deleteQuestion);
+
+router.get("/node", auth, getChallenge);
 
 export default router;
