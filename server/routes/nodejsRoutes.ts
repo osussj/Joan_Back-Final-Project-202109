cb54bca9e697255fe203ac7a4d35f0aedcb8077e
osu <joanosuna9@gmail.com>
2021-11-27 13:32:27 +0100

    Added /profiles endpoint to nodejsRoutes

--- server/routes/nodejsRoutes.ts
+++ server/routes/nodejsRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 
-import loginUserNode from "../controller/userNodeController";
+import { loginUserNode, getUserNode } from "../controller/userNodeController";
 
 const router = express.Router();
 
 router.post("/login", loginUserNode);
+router.get("/profiles", getUserNode);
 
 export default router;
