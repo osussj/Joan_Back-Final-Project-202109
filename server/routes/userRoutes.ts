f9f054d4efdca3d9148640c65fbcc9d25714968d
osu <joanosuna9@gmail.com>
2021-11-27 12:40:15 +0100

    Added upload and firebase middlwares to /register endpoint

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -7,10 +7,12 @@ import {
   updateUser,
 } from "../controller/userController";
 import auth from "../middlewares/auth";
+import firebase from "../middlewares/firebase";
+import upload from "../middlewares/upload";
 
 const router = express.Router();
 
-router.post("/register", createUser);
+router.post("/register", upload.single("avatar"), firebase, createUser);
 router.post("/login", loginUser);
 router.get("/profile", auth, getUser);
 router.put("/profile/update", auth, updateUser);
