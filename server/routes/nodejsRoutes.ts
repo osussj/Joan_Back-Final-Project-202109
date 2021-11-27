d35ecac686281b19188dd5de02625f56e256f714
osu <joanosuna9@gmail.com>
2021-11-27 13:52:49 +0100

    Added /users endpoint to nodejsRoutes with getAllUsers

--- server/routes/nodejsRoutes.ts
+++ server/routes/nodejsRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
-import { loginUserNode, getUserNode } from "../controller/userNodeController";
+import {
+  loginUserNode,
+  getUserNode,
+  getAllUsers,
+} from "../controller/userNodeController";
 
 const router = express.Router();
 
 router.post("/login", loginUserNode);
 router.get("/profiles", getUserNode);
+router.get("/users", getAllUsers);
 
 export default router;
