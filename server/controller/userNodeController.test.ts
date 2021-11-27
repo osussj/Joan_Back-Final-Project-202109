7171e765558cfeb280a68c502f3bb95097b7865d
osu <joanosuna9@gmail.com>
2021-11-27 13:31:43 +0100

    Added getUserNode function test of userNodecontroller

--- server/controller/userNodeController.test.ts
+++ server/controller/userNodeController.test.ts
@@ -1,7 +1,7 @@
 import { Request } from "express";
 import Usernode from "../../database/models/usernode";
 import { mockResponse } from "../../utils/mocks/mockFunction";
-import loginUserNode from "./userNodeController";
+import { getUserNode, loginUserNode } from "./userNodeController";
 
 jest.mock("../../database/models/usernode");
 jest.setTimeout(5000);
@@ -10,7 +10,7 @@ class ErrorCode extends Error {
 }
 
 describe("Given a loginUser function", () => {
-  describe("When it receives a incorrect username", () => {
+  describe("When it receives an incorrect username", () => {
     test("Then it should invoke a next function with error(code 401)", async () => {
       Usernode.findOne = jest.fn().mockResolvedValue(null);
       const req = {
@@ -82,6 +82,63 @@ describe("Given a loginUser function", () => {
       await loginUserNode(req, res, null);
 
       expect(res.json).toHaveBeenCalledWith(welcomeMessage);
+    });
+  });
+});
+
+describe("Given a getUserNode function", () => {
+  describe("When it receives an incorrect username", () => {
+    test("Then it should invoke a next function with error 404", async () => {
+      Usernode.findOne = jest.fn().mockResolvedValue(null);
+      const req = {
+        body: {
+          username: "guest",
+        },
+      } as Request;
+      const next = jest.fn();
+      const expectedError = new ErrorCode("User not found");
+      expectedError.code = 404;
+
+      await getUserNode(req, null, next);
+
+      expect(next.mock.calls[0][0]).toHaveProperty("code", expectedError.code);
+      expect(next.mock.calls[0][0]).toHaveProperty(
+        "message",
+        expectedError.message
+      );
+    });
+  });
+  describe("When it receives a correct username", () => {
+    test("Then it should invoke the method json with the user information", async () => {
+      const fakeUser = {
+        username: "guest",
+        is_admin: false,
+        password: "fakePassword",
+        id: "12",
+      };
+      const res = mockResponse();
+      const req = {
+        body: {
+          username: fakeUser.username,
+        },
+      } as Request;
+
+      Usernode.findOne = jest.fn().mockResolvedValue(fakeUser);
+      await getUserNode(req, res, null);
+
+      expect(res.json).toHaveBeenCalledWith(fakeUser);
+    });
+  });
+  describe("When the promise is rejected", () => {
+    test("Then it should call the next function", async () => {
+      const next = jest.fn();
+      const req = {
+        body: {},
+      } as Request;
+      Usernode.findOne = jest.fn().mockRejectedValue(null);
+      await getUserNode(req, null, next);
+
+      expect(next).toHaveBeenCalled();
     });
   });
 });
