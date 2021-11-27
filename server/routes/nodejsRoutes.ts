2e83409d7ab78d9349d7553fd30698d47f4a0dda
osu <joanosuna9@gmail.com>
2021-11-27 14:37:25 +0100

    Added /users/latest endpoint to nodejsRoutes

--- server/routes/nodejsRoutes.ts
+++ server/routes/nodejsRoutes.ts
@@ -4,6 +4,7 @@ import {
   loginUserNode,
   getUserNode,
   getAllUsers,
+  getLatestUsers,
 } from "../controller/userNodeController";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/login", loginUserNode);
 router.get("/profiles", getUserNode);
 router.get("/users", getAllUsers);
+router.get("/users/latest", getLatestUsers);
 
 export default router;
